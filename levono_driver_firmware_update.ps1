59d156211afc46d4a7cc984d72f15d2356b8f6d1
edbat <edwardbattye@gmail.com>
2023-02-24 11:00:43 +0000

    Create levono_driver_firmware_update.ps1
    Install LSU Client and install all available driver updates for Levono laptops. If Bios Update is due reboot or shutdown depending on what is required.

---
+++ levono_driver_firmware_update.ps1
@@ -0,0 +1,27 @@
+﻿##Install LSUClient Module
+
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Set-ExecutionPolicy Unrestricted -Force
+Import-Module PowerShellGet
+Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
+Install-Module -Name 'LSUClient'
+Import-Module LSUClient
+
+##Find updates and install, if Bios update create 
+
+$updates = Get-LSUpdate 
+$updates | Install-LSUpdate -SaveBIOSUpdateInfoToRegistry 
+
+##Check registry to see if reboot or shutdown required, then action and clean up regsitry key 
+
+$bios_action = Get-ItemPropertyValue -Path "HKLM:\Software\LSUClient\BIOSUpdate" -Name ActionNeeded
+If($bios_action -eq "REBOOT") {
+    Write-Host "Rebooting to apply Bios updates"
+    Shutdown.exe /r /t 15
+    Remove-ItemProperty -Path "HKLM:\Software\LSUClient\BIOSUpdate" -Name ActionNeeded -Force
+}
+    ElseIf($bios_action -eq "SHUTDOWN") {
+        Write-Host "Shutting down to apply Bios Updates"
+        Shutdown.exe /s /t 15
+        Remove-ItemProperty -Path "HKLM:\Software\LSUClient\BIOSUpdate" -Name ActionNeeded -Force
+        }
